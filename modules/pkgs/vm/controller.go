88d71836622de84ae9fc48faed1c12882b3b7f37
Santtu Lakkala <santtu.lakkala@unikie.com>
2026-05-21 10:24:19 +0300

    VM memory size control
    Signed-off-by: Santtu Lakkala <santtu.lakkala@unikie.com>

---
+++ modules/pkgs/vm/controller.go
@@ -0,0 +1,102 @@
+// SPDX-FileCopyrightText: 2024-2026 TII (SSRC) and the Ghaf contributors
+// SPDX-License-Identifier: Apache-2.0
+package vm
+
+import (
+	"context"
+	"fmt"
+	"regexp"
+
+	givc_vm "givc/modules/api/vm"
+
+	"github.com/godbus/dbus/v5"
+	log "github.com/sirupsen/logrus"
+)
+
+type VmController struct {
+	conn *dbus.Conn
+	re   *regexp.Regexp
+}
+
+func NewController() (*VmController, error) {
+	var err error
+	var c VmController
+
+	c.conn, err = dbus.ConnectSystemBus()
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to system bus: %s", err)
+	}
+
+	c.re = regexp.MustCompile("[^A-Za-z0-9]")
+
+	return &c, nil
+}
+
+func (c *VmController) Close() {
+	err := c.conn.Close()
+	if err != nil {
+		log.Warnf("[VmController] failed to close connection: %s", err)
+	}
+}
+
+func (c *VmController) objectPath(vm string) dbus.ObjectPath {
+	vmPath := "/" + c.re.ReplaceAllLiteralString(vm, "_")
+	return dbus.ObjectPath(vmPath)
+}
+
+func (c *VmController) object(vm string) dbus.BusObject {
+	vmObj := c.objectPath(vm)
+	return c.conn.Object("ae.tii.MemManager", vmObj)
+}
+
+func (c *VmController) VMSize(ctx context.Context, vm string, minimum *uint64, maximum *uint64) (*givc_vm.VMSizeResponse, error) {
+	vmObj := c.object(vm)
+	var ret givc_vm.VMSizeResponse
+	if minimum != nil {
+		if err := vmObj.SetProperty("ae.tii.MemManager.VM.Minimum", *minimum); err != nil {
+			return nil, err
+		}
+		ret.Minimum = *minimum
+	} else {
+		val, err := vmObj.GetProperty("ae.tii.MemManager.VM.Minimum")
+		if err != nil {
+			return nil, err
+		}
+		uval, _ := val.Value().(uint64)
+		ret.Minimum = uval
+	}
+	if maximum != nil {
+		if err := vmObj.SetProperty("ae.tii.MemManager.VM.Maximum", *maximum); err != nil {
+			return nil, err
+		}
+		ret.Maximum = *maximum
+	} else {
+		val, err := vmObj.GetProperty("ae.tii.MemManager.VM.Maxmium")
+		if err != nil {
+			return nil, err
+		}
+		uval, _ := val.Value().(uint64)
+		ret.Maximum = uval
+	}
+
+	val, err := vmObj.GetProperty("ae.tii.MemManager.VM.MaxSize")
+	if err != nil {
+		return nil, err
+	}
+	uval, _ := val.Value().(uint64)
+	ret.Size = uval
+
+	return &ret, nil
+}
+
+func (c *VmController) VMStats(ctx context.Context, vm string) (*map[string]uint64, error) {
+	vmObj := c.object(vm)
+	var stats map[string]uint64
+
+	err := vmObj.Call("ae.too.MemManager.VM.GetStats", 0).Store(&stats)
+	if err != nil {
+		return nil, err
+	}
+
+	return &stats, nil
+}
